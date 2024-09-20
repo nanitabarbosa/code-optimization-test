e9aa6f881825ce52723ef3a34559c7822b6f7cc8
Mariana <mbarbosat@ufpso.edu.co>
2024-09-20 01:34:06 -0500

    actualizaciones

--- src/Store.java
+++ src/Store.java
@@ -1,28 +1,31 @@
 public class Store {
+
     public static void main(String[] args) {
         final int RICE = 15;
         final int COFFEE = 10;
         final int MEAT = 5; //Se esta cambiando los numeros magicos, por constantes
+        int [] price = {RICE, COFFEE, MEAT};
 
         final int AMOUNT_RICE= 2;
         final int AMOUNT_COFFEE= 3;
         final int AMOUNT_MEAT= 4; //se esta cambiando los numero magicos, por constantes
+        int [] products = {AMOUNT_RICE, AMOUNT_COFFEE, AMOUNT_MEAT};
+        
         final int SALES = 50;
+        int totalSales=0; //se inicializo un sumador en 0
+        
 
-        int [][] total = new int[3][3];
-        for (int i=0; i < 4; i++){
-            for (int j=0; j < 4; j++){
-                System.out.println("El producto " + (i+1) + );
-            }
+        for (int i=0; i < products.length; i++){ //se inicializo un for para correr los precios y los productos en posicion i, 
+            //y que su muktiplicacion ve vaya sumando en la variable totalSales
+
+            totalSales += price[i] * products[i];
         }
 
-
-        int totalSales = total1 + total2 + total3;
-
-        if (totalSales > 50) {
+        if (totalSales > SALES) { //Si suma que recolecto totalSales
+            //es mayor a 50 se imprime el primer mensaje, y si no lo es, se imprime el segundo
             System.out.println("Good sales performance");
         } else {
-            System.out.println("Low sales performance");
+            System.out.println("Low s performance");
         }
     }
 }
--- src/TaxCalculation.java
+++ src/TaxCalculation.java
@@ -1,25 +1,27 @@
 public class TaxCalculation {
     public static void main(String[] args) {
-        double productPrice1 = 100;
-        double productPrice2 = 200;
-        final double TAX_RATE_HIGH = productPrice1 * 0.15;
-        final double TAX_RATE_LOW = productPrice2 * 0.10;
+        double productprice1 = 100;
+        double productprice2= 200;
+        final double TAX_RATE_HIGH = 0.15;
+        final double TAX_RATE_LOW =  0.10;
         final int TAX_THRESHOLD = 50; //estoy cambiando los numeros magicos por constantes 
 
-        double totalTax = TAX_RATE_HIGH + TAX_RATE_LOW;
+        double [] price = {productprice1, productprice2};
+        double [] taxs = {TAX_RATE_HIGH, TAX_RATE_LOW};
 
-        int[][] taxes = new int[2][2];
+        double totalTax = 0;
         
-        for (int i=0; i < 10; i++){
+        for (int i=0; i < price.length; i++){
 
-            if (totalTax > TAX_THRESHOLD) {
-                System.out.println("High total tax: " + totalTax);
-            } else {
-                System.out.println("Low tax");
-            }
+            totalTax += price[i] * taxs[i];
         }
-        productPrice1
-        
+
+        if (totalTax > TAX_THRESHOLD) {
+            System.out.println("High total tax: " + totalTax);
+        } else {
+            System.out.println("Low tax");
+        }
+
        
     }
 }
